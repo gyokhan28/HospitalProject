362b42351e53a2da9a0fdb939387d74760ae910e
Gyokhan <gyokhan28@gmail.com>
2023-11-06 20:21:42 +0200

    added Doctor/Patient load classes & methods.

--- src/LoginPatient.java
+++ src/LoginPatient.java
@@ -5,38 +5,36 @@ import java.util.List;
 import java.util.Scanner;
 
 public class LoginPatient {
-    public class LoginDoctor {
-        PatientFileManager patientFileManager;
-        protected List<Patient> patientList;
+    PatientFileManager patientFileManager;
+    protected List<Patient> patientList;
 
-        public LoginDoctor() throws IOException {
-            patientFileManager = new PatientFileManager();
-            patientList = patientFileManager.loadDoctors("Patients.csv");
-            verify();
-        }
+    public LoginPatient() throws IOException {
+        patientFileManager = new PatientFileManager();
+        patientList = patientFileManager.loadPatients("Patients.csv");
+        verify();
+    }
 
-        protected void verify() {
-            System.out.println("Enter patient ID:");
-            Scanner sc = new Scanner(System.in);
-            int id= -1;
-            boolean formatFlag = false;
-            while (!formatFlag) {
-                try {
-                    id = sc.nextInt();
-                    formatFlag = true;
-                } catch (InputMismatchException e) {
-                    System.out.println("You must enter an integer!");
-                    sc.next();
-                }
+    protected void verify() {
+        System.out.println("Enter patient ID:");
+        Scanner sc = new Scanner(System.in);
+        int id = -1;
+        boolean formatFlag = false;
+        while (!formatFlag) {
+            try {
+                id = sc.nextInt();
+                formatFlag = true;
+            } catch (InputMismatchException e) {
+                System.out.println("You must enter an integer!");
+                sc.next();
             }
-            System.out.println("Enter name:");
-            sc.nextLine();
-            String name = sc.nextLine();
-            for(Patient patientsList:patientList) {
-                if (id==(patientsList.getId())&& name.equalsIgnoreCase(patientsList.getFirstName())) {
-                    System.out.println("Welcome "+patientsList.getFirstName());
-                    return;
-                }
+        }
+        System.out.println("Enter name:");
+        sc.nextLine();
+        String name = sc.nextLine();
+        for (Patient patientsList : patientList) {
+            if (id == (patientsList.getId()) && name.equalsIgnoreCase(patientsList.getFirstName())) {
+                System.out.println("Welcome " + patientsList.getFirstName());
+                return;
             }
         }
     }
