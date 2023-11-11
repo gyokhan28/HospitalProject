8c880c63240e08ff7af0c067f5f3d08bcd765aaa
Redjebow <135421535+Redjebow@users.noreply.github.com>
2023-11-11 17:47:48 +0200

    Add new class AppointmentAdder

---
+++ src/AppointmentAdder.java
@@ -0,0 +1,107 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
+public class AppointmentAdder {
+    // private final int personalPatientId;
+    private final List<Doctor> doctorsList;
+    private List<Appointment> appointmentList;
+    private final List<Patient> patientList;
+
+    public AppointmentAdder() throws IOException {
+        DoctorFileManager doctorFileManager = new DoctorFileManager();
+        //AppointmentsFileManager appointmentsFileManager = new AppointmentsFileManager();
+        PatientFileManager patientFileManager = new PatientFileManager();
+        doctorsList = doctorFileManager.loadDoctors("Doctors.csv");
+        // appointmentList = appointmentsFileManager.loadAppointments("Appointments.csv");
+        patientList = patientFileManager.loadPatients("Patients.csv");
+        // this.doctorList = doctorList;
+        // this.personalPatientId = id;
+    }
+
+
+    public void addNewHour(int iDPatient) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("До тук добре ..." + iDPatient);
+        int lastID = returnLastID();
+        String examinationType = returnTypeOfExamination();
+        System.out.print("Enter date of examination: ");
+        String date = sc.nextLine();
+        System.out.print("Enter time of examination:");
+        int time = sc.nextInt();
+        int doctorID = returnDoctorID();
+        System.out.println("Last ID" + lastID + " Examination type " + examinationType + " Date " + date + " time " + time + " doctor ID" + doctorID);
+        // Appointment appointment = new Appointment(lastID+1,iDPatient,examinationType,date,time,doctorID);
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("Appointments copy.csv", true))) {
+
+            bw.write(returnLastID() + 1 + "," + iDPatient + "," + examinationType + "," + date + "," + time + "," + doctorID);
+            bw.newLine();
+
+            System.out.println("File is updated!");
+        } catch (IOException e) {
+            System.err.println("Error writing in file: " + e.getMessage());
+        }
+
+    }
+
+    public String returnTypeOfExamination() {
+        System.out.println("Please select the type of doctor's examination: ");
+        System.out.println("1. Initial.");
+        System.out.println("2. Secondary");
+        System.out.println("3. Consultation");
+        System.out.println("4. Procedure");
+        Scanner sc = new Scanner(System.in);
+        int choice = 0;
+        boolean formatFlag = false;
+        while (!formatFlag) {
+            try {
+                choice = sc.nextInt();
+                formatFlag = true;
+            } catch (InputMismatchException e) {
+                System.out.println("You must enter an integer!");
+                sc.next();
+            }
+        }
+        do {
+            switch (choice) {
+                case 1 -> {
+                    return "Initial";
+                }
+                case 2 -> {
+
+                    return "Secondary";
+                }
+                case 3 -> {
+                    return "Consultation";
+                }
+                case 4 -> {
+                    return "Procedure";
+                }
+                default -> System.out.print("Wrong input! Try again: ");
+            }
+        } while (true);
+    }
+
+    public int returnDoctorID() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please enter the name of the doctor you wish to see: ");
+        String doctorName = sc.next();
+        for (Doctor doctor : doctorsList) {
+            if (doctorName.equalsIgnoreCase(doctor.getFirstName())) {
+                return doctor.getId();
+            }
+        }
+        System.out.println("There is no doctor with that name. Please, try again");
+        return returnDoctorID();
+    }
+
+    public int returnLastID() throws IOException {
+        AppointmentsFileManager appointmentsFileManager = new AppointmentsFileManager();
+        appointmentList = appointmentsFileManager.loadAppointments("Appointments copy.csv");
+        return appointmentList.get(appointmentList.size() - 1).getAppointmentId();
+    }
+}
--- src/PatientMenu.java
+++ src/PatientMenu.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -7,7 +8,7 @@ public class PatientMenu {
 
     private List<Patient> patientList;
 
-    public PatientMenu(int id, String firstName, String lastName, List<Patient> patientList) {
+    public PatientMenu(int id, String firstName, String lastName, List<Patient> patientList) throws IOException {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
@@ -16,7 +17,7 @@ public class PatientMenu {
 
     }
 
-    public void showMenu() {
+    public void showMenu() throws IOException {
         System.out.println("Logged in as " + firstName + " " + lastName + "!");
         System.out.println("1. Book a new doctor's appointment.");
         System.out.println("2. View of recorded hours for a patient.");
@@ -25,14 +26,18 @@ public class PatientMenu {
         chooseAnOption();
     }
 
-    public void chooseAnOption() {
+    public void chooseAnOption() throws IOException {
         Scanner sc = new Scanner(System.in);
         int choice = 0;
         System.out.print("Enter your choice: ");
         do {
             choice = sc.nextInt();
             switch (choice) {
-                case 1 -> System.out.println("1. Book a new doctor's appointment.");
+                case 1 -> {
+                    System.out.println("1. Book a new doctor's appointment.");
+                    AppointmentAdder appointmentAdder = new AppointmentAdder();
+                    appointmentAdder.addNewHour(id);
+                }
                 case 2 -> System.out.println("2. View of recorded hours for a patient.");
                 case 3 -> System.out.println("3. Change the date/time of a recorded appointment.");
                 case 4 -> System.out.println("4. Canceling an appointment.");
