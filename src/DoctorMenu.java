112257730aeb869cf302a2442d50a24557126179
Gyokhan <gyokhan28@gmail.com>
2023-11-16 22:27:28 +0200

    updated menu for patients with no appoinments (in patient menu class)

--- src/DoctorMenu.java
+++ src/DoctorMenu.java
@@ -7,8 +7,8 @@ public class DoctorMenu {
     private final AppointmentSorter appointmentSorter;
 
     public DoctorMenu(int id, String firstName, String lastName, List<Doctor> doctorList) throws IOException {
-        appointmentDisplayer = new AppointmentDisplayer(id,doctorList);
-        appointmentSorter = new AppointmentSorter(id,doctorList);
+        appointmentDisplayer = new AppointmentDisplayer(id, doctorList);
+        appointmentSorter = new AppointmentSorter(id, doctorList, firstName, lastName);
         System.out.println("\nLogged in as " + firstName + " " + lastName + "!");
     }
 
@@ -18,26 +18,27 @@ public class DoctorMenu {
         System.out.println("3.Group patients\n");
         selectMenuOption();
     }
+
     public void selectMenuOption() throws IOException {
         Scanner sc = new Scanner(System.in);
-        int choice;
+        String choice;
         System.out.print("Enter your choice: ");
         do {
-            choice = sc.nextInt();
+            choice = sc.nextLine();
             switch (choice) {
-                case 1:
+                case "1":
                     appointmentDisplayer.loadAppointmentsForSelectedDoctor();
                     showMenu();
                     break;
-                case 2:
+                case "2":
                     appointmentSorter.sortChoice();
                     break;
-                case 3:
+                case "3":
                     System.out.println("3.Group");
                     break;
                 default:
                     System.out.print("Wrong input! Try again: ");
             }
-        } while (choice != 1 && choice != 2 && choice != 3);
+        } while (!choice.equals("1") && !choice.equals("2") && !choice.equals("3"));
     }
 }
