bd308fbab93a4a996f217ae7c05a9df44d7fc826
Gyokhan <gyokhan28@gmail.com>
2023-12-07 21:55:20 +0200

    Fixed CancelingByAppointmentId

---
+++ test/LoginDoctorTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class LoginDoctorTest {
+    @BeforeEach
+    public void setup() throws IOException {
+        Setup.loadLists();
+    }
+
+    @Test
+    public void testReturnDoctorByGivenId() {
+        //GIVEN
+        int id = 3;
+
+        //WHEN
+        Doctor doctor = LoginDoctor.returnDoctor(id);
+
+        //THEN
+        Doctor expectedDoctor = Setup.getDoctorList().get(id - 1);
+        Assertions.assertEquals(doctor, expectedDoctor);
+    }
+
+}
